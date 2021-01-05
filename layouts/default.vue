adb7f064635bda51ec7748b9e78ec8a7320b0da5
subhadeep.chel <chelsubhadeep@gmail.com>
2021-01-05 20:36:31 +0530

    create weather app page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,19 @@
 <template>
   <v-app>
     <v-main>
-      <div style="background-color: '#fff'">
-        <v-toolbar color="indigo" dark>
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
-          <v-toolbar-title>NuxtJs Tutorial</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-toolbar-items class="hidden-sm-and-down">
-            <v-btn text><n-link to="/weather-app">Weather App</n-link></v-btn>
-          </v-toolbar-items>
-        </v-toolbar>
-        <transition>
+      <v-toolbar color="indigo" dark>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>NuxtJs Tutorial</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn text><n-link to="/weather-app">Weather App</n-link></v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <transition>
+        <v-main>
           <nuxt />
-        </transition>
-      </div>
+        </v-main>
+      </transition>
     </v-main>
   </v-app>
 </template>
---
+++ pages/weather-app.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <!--<v-layout>-->
+    <v-flex xs12>
+      <h1 class="display-1 text-center">Weather App</h1>
+      <v-card color="blue-grey darken-2" dark>
+        <v-card-text class="white--text"> Here is the card </v-card-text>
+      </v-card>
+    </v-flex>
+
+    <v-flex xs12 class="mt-4">
+      <v-text-field label="Another input" solo></v-text-field>
+    </v-flex>
+    <!--</v-layout>-->
+  </v-container>
+</template>
